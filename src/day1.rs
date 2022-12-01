19d04cea4de62a99b97b30ab0b774fddfb4c253f
peterdijkstra <peter@crossup.tech>
2022-12-01 19:53:58 +0100

    removed calorie_iter variable
    because I just can just add to the last element in the vector. what was I thinking

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,13 @@
 pub fn day1() {
     let lines = include_str!("day1.txt").lines();
     let mut calories = vec![1i32, 0];
-    let mut calorie_iter = 0;
     for line in lines {
         if line.is_empty() {
-            calorie_iter += 1;
             calories.push(0);
         } else {
             let calorie_amount = line.parse::<i32>().unwrap();
-            calories[calorie_iter] += calorie_amount;
+            let last_iter = calories.len() - 1;
+            calories[last_iter] += calorie_amount;
         }
     }
 
