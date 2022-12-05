28fa9226d56413c98e0b9cd98e2a3802220b91d2
peterdijkstra <peter@crossup.tech>
2022-12-05 18:00:19 +0100

    day 3 part 1 without external crate

--- src/day3.rs
+++ src/day3.rs
@@ -1,18 +1,30 @@
-extern crate array_tool;
-use array_tool::vec::*;
-
 pub fn day3() {
     let lines = include_str!("day3.txt").lines();
-	let mut sum = 0;
+    let mut sum = 0;
     for line in lines {
         let split = line.split_at(line.len() / 2);
         let vec_a: Vec<char> = split.0.chars().collect();
         let vec_b: Vec<char> = split.1.chars().collect();
-        let intersect = vec_a.intersect(vec_b);
-        let c = intersect[0];
-        let is_upper = c.is_uppercase();
-        let value = c as u32 - if is_upper { 38 } else { 96 };
-		sum += value;
+
+        for c_a in vec_a {
+            let mut should_break = false;
+            for c_b in &vec_b {
+                if c_a == *c_b {
+                    println!("match {}", c_a);
+                    sum += calc_score(&c_a);
+                    should_break = true;
+                    break;
+                }
+            }
+            if should_break {
+                break;
+            }
+        }
     }
-	println!("SUM: {}", sum);
+    println!("SUM: {}", sum);
+}
+
+fn calc_score(c: &char) -> u32 {
+    let is_upper = c.is_uppercase();
+    *c as u32 - if is_upper { 38 } else { 96 }
 }
