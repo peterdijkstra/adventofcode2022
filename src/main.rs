eef5f460425a1f9b443db18ef911b0f926d48afa
peterdijkstra <peter@crossup.tech>
2022-12-08 11:15:21 +0100

    main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use crate::day1::day1;
 use crate::day2::day2;
 use crate::day3::day3;
+use crate::day4::day4;
 
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     println!("Hello, Advent of Code!");
     // day1();
     // day2();
-    day3();
+    // day3();
+    day4();
 }
