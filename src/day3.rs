81eb33ce32cab2207f1fa3601827830b3e90a562
peterdijkstra <peter@crossup.tech>
2022-12-08 10:17:18 +0100

    stuck on day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,9 @@
+use std::collections::{hash_map, HashMap};
+
 pub fn day3() {
-    let lines = include_str!("day3.txt").lines();
+    let lines: Vec<&str> = include_str!("day3.txt").lines().collect();
     let mut sum = 0;
-    for line in lines {
+    for line in &lines {
         let split = line.split_at(line.len() / 2);
         let vec_a: Vec<char> = split.0.chars().collect();
         let vec_b: Vec<char> = split.1.chars().collect();
@@ -22,6 +24,51 @@ pub fn day3() {
         }
     }
     println!("SUM: {}", sum);
+
+    let mut sum_2 = 0;
+
+    for window in lines.windows(3) {
+        println!("{}", "\nnew window!");
+        let mut group_map: HashMap<char, u8> = HashMap::new();
+        // let mut group_index = 0;
+        for w in window {
+            let string = *w;
+            // println!("{}", string);
+            let mut chars: Vec<char> = string.chars().collect();
+            chars.sort();
+            chars.dedup();
+            let uniq: String = chars.iter().collect();
+            println!("deduped: {}", uniq);
+            for c in chars {
+                if group_map.contains_key(&c) {
+                    // if group_map[&c] == group_index {
+                    //     continue;
+                    // }
+                    if let Some(x) = group_map.get_mut(&c) {
+                        *x += 1;
+                    }
+                    continue;
+                }
+                group_map.insert(c, 0);
+            }
+            // group_index += 1;
+        }
+
+        for kv in group_map {
+            if kv.1 == 2 {
+                println!("common: {}", kv.0);
+                sum_2 += calc_score(&kv.0);
+            }
+        }
+
+        // if let Some(max) = group_map.keys().max() {
+        //     println!("common: {}, amount: {}", max, group_map.get(max).unwrap());
+        //     sum_2 += calc_score(max);
+        // }
+        
+    }
+
+    println!("SUM 2: {}", sum_2);
 }
 
 fn calc_score(c: &char) -> u32 {
