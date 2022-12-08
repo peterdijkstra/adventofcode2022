0b6fe91f052c6bd5d569a13fc08a6a8738d9483f
peterdijkstra <peter@crossup.tech>
2022-12-08 11:15:13 +0100

    day 4 part 1

---
+++ src/day4.rs
@@ -0,0 +1,22 @@
+use std::ops::Range;
+
+pub fn day4() {
+	let lines = include_str!("day4.txt").lines();
+
+	let mut contains = 0;
+
+	for line in lines {
+		let split: Vec<&str> = line.split(',').collect();
+		let range_a_values: Vec<u32> = split[0].split('-').map(|x| x.parse::<u32>().unwrap()).collect();
+		let range_b_values: Vec<u32> = split[1].split('-').map(|x| x.parse::<u32>().unwrap()).collect();
+		// let range_a = [range_a_values[0]..range_a_values[1]];
+		// let range_b = [range_b_values[0]..range_b_values[1]];
+
+		if range_a_values[0] >= range_b_values[0] && range_a_values[1] <= range_b_values[1] ||
+		range_b_values[0] >= range_a_values[0] && range_b_values[1] <= range_a_values[1] {
+			contains += 1;
+		}
+	}
+
+	println!("CONTAINS: {}", contains);
+}
