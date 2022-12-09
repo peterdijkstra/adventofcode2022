38ca4b9d1956bb16bb780643a56d47b8e644e5b8
peterdijkstra <peter@crossup.tech>
2022-12-09 12:01:20 +0100

    cleanup

--- src/day5.rs
+++ src/day5.rs
@@ -3,9 +3,9 @@ pub fn day5() {
 
     let stack_data: Vec<&str> = lines.clone().take_while(|l| l.len() != 0).collect();
 
-    for data in &stack_data {
-        println!("{}", data);
-    }
+    // for data in &stack_data {
+    //     println!("{}", data);
+    // }
 
     let move_data: Vec<&str> = lines.skip_while(|l| l.len() != 0).skip(1).collect();
 
@@ -13,31 +13,29 @@ pub fn day5() {
     // 	println!("{}", data);
     // }
 
-    let mut stacks: Vec<Vec<char>> = Vec::new();
-
-    let mut stack_data_iter = stack_data.iter().map(|l| 
-		l.chars().skip(1).step_by(2).step_by(2).collect::<String>());
+    let mut stack_data_iter = stack_data
+        .iter()
+        .map(|l| l.chars().skip(1).step_by(2).step_by(2).collect::<String>());
 
     let last = stack_data_iter.next_back().unwrap();
 
-	dbg!(last);
+    let stack_amount = last.chars().last().unwrap().to_digit(10).unwrap() as usize;
+    let mut stacks_1: Vec<Vec<char>> = vec![Vec::new(); stack_amount];
 
     for line in stack_data_iter {
         for (i, c) in line.chars().enumerate() {
-            let mut stack = stacks.get_mut(i);
-            if stack.is_none() {
-                stacks.push(Vec::new());
-                stack = stacks.get_mut(i);
-            }
+            let stack = stacks_1.get_mut(i);
             if c != ' ' {
                 stack.unwrap().insert(0, c);
             }
         }
     }
 
-    for (i, s) in stacks.iter().enumerate() {
+    for (i, s) in stacks_1.iter().enumerate() {
         println!("STACK {}: {}", i, s.iter().collect::<String>());
     }
+
+	let mut stacks_2 = stacks_1.clone();
 
     for line in move_data {
         let nums = line
@@ -53,25 +51,23 @@ pub fn day5() {
         let from = split[1] - 1;
         let to = split[2] - 1;
 
+		// part 1
         for _ in 0..split[0] {
             let mut take_char: Option<char> = None;
-
             {
-                let take = stacks.get_mut(from).unwrap();
+                let take = stacks_1.get_mut(from).unwrap();
                 take_char = Some(take.pop().unwrap());
             }
 
             if let Some(c) = take_char {
-                let push = stacks.get_mut(to).unwrap();
+                let push = stacks_1.get_mut(to).unwrap();
                 push.push(c);
             }
         }
 
-        // for (i, s) in stacks.iter().enumerate() {
-        // 	println!("STACK {}: {}", i, s.iter().collect::<String>());
-        // }
+		// part 2
     }
 
-    let answer = stacks.iter().map(|s| s.last().unwrap()).collect::<String>();
+    let answer = stacks_1.iter().map(|s| s.last().unwrap()).collect::<String>();
     println!("ANSWER: {}", answer); // should be TWSGQHNHL
 }
